890d7dcd631ed99e20f950cf252a74512a3acb44
Emre Can Hacıosmanoğlu <emrecan.haciosmanoglu@agu.edu.tr>
2020-08-17 19:28:27 +0300

    Linux Support

--- premake5.lua
+++ premake5.lua
@@ -1,8 +1,6 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-	cppdialect "C++17"
-	staticruntime "On"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -23,6 +21,8 @@ project "ImGui"
 
 	filter "system:windows"
 		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
 
 	filter "system:linux"
 		pic "On"
